336127d91883a7caa2c2685467045f73774113cf
ALEXX <alexedgik@gmail.com>
2022-08-11 20:37:34 +0300

    FragmentApp

---
+++ app/src/main/java/com/example/fragmentapp/BlankFragment2.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
+import com.example.fragmentapp.databinding.Fragment2Binding
+
+class BlankFragment2 : Fragment() {
+    private val dataModel: DataModel by activityViewModels()
+    lateinit var binding: Fragment2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = Fragment2Binding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dataModel.messageForFrag2.observe(activity as LifecycleOwner) {
+            binding.tvMessage.text = it
+        }
+        binding.bSendToFrag1.setOnClickListener {
+            dataModel.messageForFrag1.value = "Hello from Fragment 2"
+        }
+        binding.bSendToActivity.setOnClickListener {
+            dataModel.messageForActivity.value = "Hello activity from Fragment 2"
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = BlankFragment2()
+    }
+}
